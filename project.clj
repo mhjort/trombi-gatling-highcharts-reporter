c0c85169cec2d1c54d43b569dc692a5545b53d7d
Markus Hjort <markus.hjort@gmail.com>
2021-07-21 08:53:50 +0300

    Use only standard repositories

--- project.clj
+++ project.clj
@@ -12,5 +12,4 @@
                                net.sf.saxon/Saxon-HE]]
                  [io.gatling.highcharts/gatling-charts-highcharts "2.0.3"
                   :exclusions [io.gatling/gatling-app io.gatling/gatling-recorder]]]
-  :repositories { "excilys" "http://repository.excilys.com/content/groups/public" }
   :profiles {:dev {:dependencies [[clj-containment-matchers "1.0.1"]] }})
